a5192c1be4e43a54d8b6dd717ab938b9e0008dd4
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-21 01:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function highlight() {
+function highlight(e) {
     //Write your code here
-
-
+	let strongElems = document.querySelectorAll("strong");
+	strongElems.forEach((elem)=>{
+		console.log(elem.style.color = "rgb(0, 128, 0)")
+	});
 }
 
 
-function return_normal() {
-    //Write your code here
 
-    
+
+function return_normal(e) {
+    //Write your code here
+	let strongElems = document.querySelectorAll("strong");
+	strongElems.forEach((elem)=>{
+		console.log(elem.style.color = "rgb(0, 0, 0)")
+	});
 }
